d72834cc191a2025aa218b7a05df8845d492bbdc
Khaja Minhajuddin <minhajuddin@minhajuddin-desktop.(none)>
2010-01-18 17:31:02 +0530

    added the webrock.rb file which has code for a web server, copied from the internet

--- ruby/pick_axe/jukebox/song.rb
+++ ruby/pick_axe/jukebox/song.rb
@@ -1,12 +1,19 @@
 class Song
+  attr_reader :name, :artist, :duration
   def initialize(name, artist, duration)
     @name = name
     @artist = artist
     @duration = duration
   end
-end
 
-class Song
+  def duration_in_minutes
+    @duration / 60.0
+  end
+
+  def duration_in_minutes= (new_duration)
+    @duration = (new_duration * 60).to_i
+  end
+  
   def to_s
     "Song: #@name -- #@artist (#@duration)"
   end
@@ -16,6 +23,9 @@ class KaraokeSong < Song
   def initialize(name, artist, duration, lyrics)
     super(name,artist,duration)
     @lyrics=lyrics
+  end
+  def to_s
+    super + "[#@lyrics]"
   end
 end
 
